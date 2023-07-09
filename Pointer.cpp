c2b1bbc7312f1eafb9ba9aca683bdd22ea7b5932
Mirza Hamza Nadeem <98513834+MIRZA-HAMZA341@users.noreply.github.com>
2023-07-09 08:10:37 -0700

    Add files via upload

---
+++ Pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int  main()
+{
+	int a=5;
+	int* b;
+	b=&a;
+	cout<<"THE ADDRESS OF A IS "<<&a<<endl;
+	cout<<"THE VALUE AT ADDRESS B IS = "<<*b<<endl;
+	
+	
+	
+	return 0;
+}
